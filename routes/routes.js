c36cfc9d92bfc34f116174d040b29be25f0c02bd
Esteban Lorca <enlorca@uc.cl>
2024-01-24 15:40:05 -0300

    ADD Se agrega GET / que sirve el HTML

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const path = require("path");
 
 const repertorioRuta = "./src/data/repertorio.json";
 
@@ -21,6 +22,23 @@ router.get("/canciones", (req, res) => {
   res.json(repertorio);
 });
 
+/* GET / (servir el HTML) */
+router.get("/", (req, res) => {
+  console.log("Solicitud recibida para /");
+  const indexPath = path.join(__dirname, "../index.html");
+  try {
+    const indexHtml = fs.readFileSync(indexPath, "utf-8");
+
+    res.header("Content-Type", "text/html");
+
+    res.send(indexHtml);
+    console.log("Respuesta enviada correctamente");
+  } catch (error) {
+    console.error("Error leyendo index.html:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 /* POST /canciones */
 router.post("/canciones", (req, res) => {
   const repertorio = getRepertorio();
@@ -29,31 +47,35 @@ router.post("/canciones", (req, res) => {
   repertorio.push(nuevaCancion);
   writeRepertorio(repertorio);
 
-  res.json({ message: "Canción agregada correctamente", cancion: nuevaCancion });
+  res.json({
+    message: "Canción agregada correctamente",
+    cancion: nuevaCancion,
+  });
 });
 
 /* PUT /canciones/:id */
 router.put("/canciones/:id", (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const cancionActualizada = req.body;
   const repertorio = getRepertorio();
-  const index = repertorio.findIndex(cancion => cancion.id == id);
+  const index = repertorio.findIndex((cancion) => cancion.id == id);
   repertorio[index] = cancionActualizada;
   writeRepertorio(repertorio);
   console.log(repertorio);
-  res.json({ message: "Canción actualizada correctamente", cancion: cancionActualizada });
+  res.json({
+    message: "Canción actualizada correctamente",
+    cancion: cancionActualizada,
+  });
 });
-
 
 /* DELETE /canciones/:id */
 router.delete("/canciones/:id", (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const repertorio = getRepertorio();
-  const index = repertorio.findIndex(cancion => cancion.id === id);
+  const index = repertorio.findIndex((cancion) => cancion.id === id);
   repertorio.splice(index, 1);
   writeRepertorio(repertorio);
   res.json({ message: "Canción eliminada correctamente" });
 });
-
 
 module.exports = router;
